7f1bce4b8a02621eb2a18c57875114989024cb67
John <mapeng712@gmail.com>
2015-12-27 20:55:26 +0800

    Initial Commit

---
+++ MPWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MPWeiBo
+//
+//  Created by John on 15/12/27.
+//  Copyright © 2015年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MPWeiBo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MPWeiBo
+//
+//  Created by John on 15/12/27.
+//  Copyright © 2015年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
