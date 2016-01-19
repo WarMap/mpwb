05ad00ba502fe4fbada51bc9cd184c5723db0ef6
John <mapeng712@gmail.com>
2016-01-19 20:50:25 +0800

    remove rubbish

--- MPWeiBo/custom/FlowingMenu/FlowingMenuDelegate.h
+++ MPWeiBo/custom/FlowingMenu/FlowingMenuDelegate.h
@@ -12,16 +12,14 @@
 @class FlowingMenuTransitionManager;
 
 @protocol FlowingMenuDelegate <NSObject>
+
 - (CGFloat)flowingMenu:(FlowingMenuTransitionManager *)flowingMenu widthOfMenuView:(UIView *)menuView;
-//func flowingMenu(flowingMenu: FlowingMenuTransitionManager, widthOfMenuView menuView: UIView) -> CGFloat
 
 - (UIColor *)colorOfElasticShapeInFlowingMenu:(FlowingMenuTransitionManager *)flowingMenu;
-//func colorOfElasticShapeInFlowingMenu(flowingMenu: FlowingMenuTransitionManager) -> UIColor?
 
 - (void)flowingMenuNeedsPresentMenu:(FlowingMenuTransitionManager *)flowingMenu;
-//func flowingMenuNeedsPresentMenu(flowingMenu: FlowingMenuTransitionManager)
 
 - (void)flowingMenuNeedsDismissMenu:(FlowingMenuTransitionManager *)flowingMenu;
-//func flowingMenuNeedsDismissMenu(flowingMenu: FlowingMenuTransitionManager)
+
 @end
 
